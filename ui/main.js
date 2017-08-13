c67e1e1643244e9145e7805f5300db4cf45f54f4
jayendraIT <30789856+jayendraIT@users.noreply.github.com>
2017-08-13 12:33:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -11,3 +11,17 @@ function moveRight (){
 madi.onclick = function () {
     var interval = setInterval(moveRight, 25);
 };
+//Counter code
+var button = document.getElementById('counter');
+var counter =  0;
+button.onclick = function (){
+    
+    // Make a request to counter endpoint
+    
+    // Capture the response and store it in a variable
+    
+    // Render the variable in the correct span
+    counter = counter + 1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
