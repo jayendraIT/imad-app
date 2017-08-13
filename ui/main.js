1cf2bae23206b6e6ac46fbb07f15e694a3d5efb7
jayendraIT <30789856+jayendraIT@users.noreply.github.com>
2017-08-13 11:24:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 // move the image
 
-var img =document.getElementBiId('madi');
+var img =document.getElementById('madi');
 madi.onclick = function () {
     madi.style.marginLeft = '100px';
     };
